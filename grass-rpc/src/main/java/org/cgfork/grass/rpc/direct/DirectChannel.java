51d1dc72e470aaab59ebd1c0d9101f9e2aa26551
cg <cg.fork@gmail.com>
2015-11-09 09:53:14 +0800

    delete

---
+++ grass-rpc/src/main/java/org/cgfork/grass/rpc/direct/DirectChannel.java
@@ -0,0 +1,10 @@
+package org.cgfork.grass.rpc.direct;
+
+import org.cgfork.grass.remote.Channel;
+
+/**
+ * @author C_G <cg.fork@gmail.com>
+ * @version 1.0
+ */
+public interface DirectChannel extends Channel {
+}
